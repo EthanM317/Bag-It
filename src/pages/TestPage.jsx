1d836f827c98a0ca5860f771084ce3b9cbc42444
mattwyand <194294135+mattwyand@users.noreply.github.com>
2025-03-06 18:44:15 -0500

    fix testpage

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -4,12 +4,38 @@ import { useState, useEffect } from "react";
 import { Backend, api } from "../api";
 
 function TestPage() {
-    
-    // Backend.getUsers();
+	const [items, setItems] = useState([]);
 
-    return <div>
-        <p>i love shit</p>
-    </div>
+	useEffect(() => {
+		getItems();
+	}, []);
+
+	const getItems = () => {
+		// setItems(Backend.getClothing());
+
+		api.get("/clothing/")
+			.then((res) => res.data)
+			.then((data) => {
+				setItems(data);
+			})
+			.catch((err) => alert(err));
+	};
+
+	return (
+		<div>
+			{/* <p>i love shit</p> */}
+			{items.map((item) => (
+                <>
+                    <p>{item.name}</p>
+                    <img src={item.image} alt={item.name} style={{
+                        width: 200,
+                        height: 200,
+                        display: "inline"
+                    }} />
+                </>
+			))}
+		</div>
+	);
 }
 
 export default TestPage;
