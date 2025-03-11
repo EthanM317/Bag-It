b3e3f22af0864bf18632745dbf610f9805166170
mattwyand <194294135+mattwyand@users.noreply.github.com>
2025-03-10 21:19:00 -0400

    add dummy bags

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,15 @@
-
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import {
+	List,
+	ListItemButton,
+	ListItemText,
+	ListSubheader,
+} from "@mui/material";
+import ListItem from "@mui/material/ListItemIcon";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 
 import { api } from "../api";
 import { ACCESS_TOKEN, Url } from "../constants";
@@ -12,14 +20,14 @@ import { ACCESS_TOKEN, Url } from "../constants";
 function ProfilePage() {
 	// State vars
 	const [username, setUsername] = useState("");
-	
+
 	// Need to do this to use "navigate" function
 	const navigate = useNavigate();
-	
+
 	useEffect(() => {
 		getUserInfo();
 	}, []);
-	
+
 	// Get user information from backend
 	const getUserInfo = async () => {
 		try {
@@ -32,7 +40,14 @@ function ProfilePage() {
 	};
 
 	const addBagClicked = () => {
-		navigate(Url.HOME);
+		// navigate(Url.HOME);
+		alert("Does nothing right now");
+	};
+
+	// DEBUG create a bunch of dummy bags
+	let bags = [];
+	for (let i = 0; i < 100; i++) {
+		bags.push({ title: "Bag #" + i, subtitle: "This is bag #" + i });
 	}
 
 	return (
@@ -41,9 +56,54 @@ function ProfilePage() {
 			<div>
 				<h2>{username}'s Bags</h2>
 				<button onClick={addBagClicked}>+ Add Bag</button>
-				{
-					// Create the list of bags here
-				}
+				<List
+					sx={{
+						width: "100%",
+						maxWidth: 360,
+						bgcolor: "background.paper",
+					}}
+					component="nav"
+					aria-labelledby="nested-list-subheader"
+					subheader={
+						<ListSubheader
+							component="div"
+							id="nested-list-subheader"
+						>
+							Bags
+						</ListSubheader>
+					}
+				>
+					{bags.map((bag) => (
+						<ListItemButton>
+							<ListItemIcon>
+								<ShoppingBagOutlinedIcon />
+							</ListItemIcon>
+							<ListItemText
+								primary={bag.title}
+								secondary={bag.subtitle}
+							/>
+						</ListItemButton>
+					))}
+					{/* <ListItemButton>
+						<ListItemIcon>
+							<ShoppingBagOutlinedIcon />
+						</ListItemIcon>
+						<ListItemText
+							primary="Bag 1"
+							secondary="Your first bag."
+						/>
+					</ListItemButton>
+
+					<ListItemButton>
+						<ListItemIcon>
+							<ShoppingBagOutlinedIcon />
+						</ListItemIcon>
+						<ListItemText
+							primary="Bag 1"
+							secondary="Your second bag."
+						/>
+					</ListItemButton> */}
+				</List>
 			</div>
 		</div>
 	);
