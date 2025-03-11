d98bed43b8e035019804aa5d119b6bbef10b0095
mattwyand <194294135+mattwyand@users.noreply.github.com>
2025-03-11 12:11:18 -0400

    add link back to home screen on 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router";
+import { Url } from "../constants";
+
 function NotFoundPage() {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<h1>404 Page not found</h1>
+			<a
+				href=""
+				onClick={() => {
+					navigate(Url.HOME);
+				}}
+			>
+				Return to home page.
+			</a>
 		</>
 	);
 }
