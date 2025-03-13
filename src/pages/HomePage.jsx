385a14c7e7ea3345e1ec6d95c0664c8b22a3f9a6
mattwyand <194294135+mattwyand@users.noreply.github.com>
2025-03-13 13:03:31 -0400

    add login button to home page
    and fix styling a little bit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import TopPanelBar from "../components/TopPanelBar.jsx";
 import Title from "../components/Title.jsx";
 import "../styles/Home.css";
+import { Button } from "@mui/material";
 
 function HomePage() {
   return <>
     <TopPanelBar/>
     <Title/>
+
+    {/* We'll fix all this weird styling up later  */}
+    <div style={{
+      textAlign: "center"
+    }}>
+      <Button variant="contained">Login</Button>
+
+    </div>
   </>;
 }
 
